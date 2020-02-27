91d4b8873e96e51c2aafa9beac9e3c6ae8dc160e
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-02-27 16:36:04 -0600

    Create BankRecords.java

---
+++ BankRecords.java
@@ -0,0 +1,65 @@
+package lab2;
+
+//Author: Anthony Irizarry
+//Date: 2/27/2020
+
+public class BankRecords {
+
+	private String id;
+	private int age;
+	//Enter Sex instance field here
+	private String region;
+	private double income;
+	private boolean married;
+	private int children;
+	private boolean car;
+	private boolean save_act;
+	private boolean current_act;
+	private boolean mortgage;
+	private boolean pep;
+	
+	//Getters and Setters
+	
+	//id getter and setter
+	public String getid() {
+		return this.id;
+	}
+	
+	public void setId(String id) {
+		this.id= id;
+	}
+	
+	//age getter and setter
+	public int getAge() {
+	return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age= age;
+	}
+	
+	//region getter and setter
+	public String getRegion() {
+		return this.region;
+	}
+	
+	public void setRegion(String region) {
+		this.region= region;
+	}
+	
+	
+	//income getter and setter
+	public double setIncome() {
+		return this.income;
+	}
+	
+	public void setIncome(double income) {
+		this.income= income;
+	}
+	
+	
+	
+	
+	
+	
+}
