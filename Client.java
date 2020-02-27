dd841516bc18f46c4e3b53a3dd6c806b5d69be97
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-02-27 16:36:25 -0600

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,14 @@
+package lab2;
+
+//Author: Anthony Irizarry
+//Date: 2/27/2020
+
+public abstract class Client {
+	
+	public abstract void readData();
+	
+	public abstract void processData();
+	
+	public abstract void printData();
+
+}
