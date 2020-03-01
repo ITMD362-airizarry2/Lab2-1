943b73b4192cb7ed8828822f4652692da8e8f4b7
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-02-29 21:02:18 -0600

    Added testing

--- BankRecordsTest.java
+++ BankRecordsTest.java
@@ -3,4 +3,11 @@ package lab2;
 //Date: 2/27/2020
 public class BankRecordsTest {
 
+		public static void main(String args []) {
+			
+			BankRecords bank1 = new BankRecords();
+			bank1.readData();
+			
+		}
 }
+
